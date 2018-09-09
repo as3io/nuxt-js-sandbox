1f3ac8811f2c9f7c15b285fe81bf95701851e838
Jacob Bare <jacob@limit0.io>
2018-09-09 15:24:34 -0500

    Add basic content query and display

--- pages/content/_id/index.vue
+++ pages/content/_id/index.vue
@@ -1,20 +1,37 @@
 <template>
   <article>
-    <h1>Article</h1>
-    <small>Return <nuxt-link to="/">Home</nuxt-link></small>
+    <h1>{{ platformContent.name }}</h1>
+    <hr>
+    <h5>{{ platformContent.teaser }}</h5>
+    <hr>
+    <!-- eslint-disable-next-line vue/no-v-html -->
+    <div v-html="platformContent.body" />
   </article>
 </template>
 
 <script>
+import contentPage from '../../../gql/pages/content.graphql';
+
 export default {
-  data() {
-    return {
-      title: 'Article',
-    };
+  async asyncData({ app, params }) {
+    const { apolloProvider } = app;
+    const { defaultClient } = apolloProvider;
+
+    // @todo For some reason the bootstrap.css.map file
+    // is being passed as an ID... fix.
+    const id = Number(params.id);
+    const input = { id };
+    const variables = { input };
+
+    if (id) {
+      const query = contentPage;
+      return defaultClient.query({ query, variables }).then(res => res.data);
+    }
+    return {};
   },
   head() {
     return {
-      title: this.title,
+      title: this.platformContent.name,
     };
   },
 };
