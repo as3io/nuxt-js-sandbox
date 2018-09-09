674297576c78dd8f095f9ee6135342b9902d3d6b
Jacob Bare <jacob@limit0.io>
2018-09-09 11:25:03 -0500

    Contrived home and article page

---
+++ pages/article.vue
@@ -0,0 +1,6 @@
+<template>
+  <article>
+    <h1>Article!</h1>
+    <small>Return <nuxt-link to="/">Home</nuxt-link></small>
+  </article>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,8 @@
 <template>
-  <h1>Hello world!</h1>
+  <main>
+    <h1>Home!</h1>
+    <ul>
+      <li><nuxt-link to="/article">Article</nuxt-link></li>
+    </ul>
+  </main>
 </template>
